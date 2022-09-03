81a6776ae21bb59f90d509da01e64c7a6a9dee38
itsjoniur <ahmadamoori.dev@gmail.com>
2022-09-04 03:36:51 +0430

    add: use middlewares from middlewares package

--- api/server.go
+++ api/server.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,13 +8,19 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-redis/redis/v9"
+	"github.com/unrolled/render"
+
+	"github.com/itsjoniur/currency/internal/middlewares"
+	"github.com/itsjoniur/currency/internal/utils"
 )
 
-func StartAPI(storage *redis.Client, port string) {
+func StartAPI(logger *utils.Logger, storage *redis.Client, port string) {
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
-	r.Use(RedisMiddleware(storage))
+	r.Use(middlewares.CurrencyLogger(logger))
+	r.Use(middlewares.RedisMiddleware(storage))
+	r.Use(middlewares.RenderMiddleware(render.New()))
 	r.Use(middleware.Recoverer)
 
 	r.Get("/", RootHandler)
@@ -24,14 +29,5 @@ func StartAPI(storage *redis.Client, port string) {
 	fmt.Printf("Server running on port %s...\n", port)
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), r); err != nil {
 		log.Panicln(err)
-	}
-}
-
-func RedisMiddleware(storage *redis.Client) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			ctx := context.WithValue(req.Context(), 1, storage)
-			next.ServeHTTP(w, req.WithContext(ctx))
-		})
 	}
 }
